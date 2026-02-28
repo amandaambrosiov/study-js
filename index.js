febe74987736365a3a2c88778d83c161ac377b89
Amanda Ambrosio <amandaambrosio295@gmail.com>
2026-02-28 17:14:37 -0300

    classes, factories, bitwise, settimeout, setinterval, requestanimationgframe, fila de eventos, módulos

---
+++ index.js
@@ -0,0 +1,9 @@
+// import { helloWorld as olaMundo, multiplica } from './utilitarios.js';
+// helloWorld();
+// olaMundo();
+
+// import * as utilitarios from './utilitarios.js';
+// utilitarios.multiplica(10);
+
+// import utilitarios from './utilitarios.js';
+// utilitarios();
